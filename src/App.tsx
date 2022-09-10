890443e966f15dada1e105ab858c8b0df4edb360
eugenio <eugenio@baliotech.com>
2022-09-10 11:25:21 -0500

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
+import React from "react";
+import { Navigation } from "./routes/Navigation";
 
 function App() {
   return (
     <>
-      <h1>App </h1>
+      <Navigation />
     </>
   );
 }
