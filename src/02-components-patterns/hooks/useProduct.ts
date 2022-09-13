587224c7dc34bde0f809927d67c3e8f916a78b1c
eugenio <eugenio@baliotech.com>
2022-09-13 16:56:13 -0500

    use product hook

---
+++ src/02-components-patterns/hooks/useProduct.ts
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export const useProduct = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    const handleIncreaseBy = (value: number) => {
+        setCounter((prev) => Math.max(prev + value, 0));
+    };
+
+
+    return {
+        counter,
+        handleIncreaseBy
+    }
+}
