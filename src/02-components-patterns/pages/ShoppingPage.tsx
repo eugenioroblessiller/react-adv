0cf91d58a097b3d7118eb2219652ffed8da2c259
eugenio <eugenio@baliotech.com>
2022-09-13 16:56:34 -0500

    showing product card

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { ProductCard } from '../components/ProductCard'
 
 export const ShoppingPage = () => {
   return (
     <div>
         <h1>Shopping page</h1>
+        <ProductCard/>
     </div>
   )
 }
