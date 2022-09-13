ff87574c41deec30a46f99d362e9cb585b68cf54
eugenio <eugenio@baliotech.com>
2022-09-13 17:09:21 -0500

    product card with prodcut

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import { ProductCard } from '../components/ProductCard'
+import React from "react";
+
+import { ProductCard } from "../components/ProductCard";
+
+const product = {
+  id: "1",
+  title: "Coffe One",
+  img: './coffee-mug.png'
+};
 
 export const ShoppingPage = () => {
   return (
     <div>
-        <h1>Shopping page</h1>
-        <ProductCard/>
+      <h1>Shopping page</h1>
+      <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+        <ProductCard key={product.id} product={product} />
+      </div>
     </div>
-  )
-}
+  );
+};
