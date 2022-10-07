4343256fd81e966c9382b22957964aec1b8f7468
eugenio <eugenio@baliotech.com>
2022-10-06 22:07:55 -0500

    shopping cart

--- src/02-components-patterns/hooks/useProduct.ts
+++ src/02-components-patterns/hooks/useProduct.ts
@@ -1,5 +1,5 @@
 import { onChangeArgs, Product } from './../interfaces/interfaces';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 interface useProductArgs {
     product: Product,
@@ -11,7 +11,12 @@ export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => 
 
     const [counter, setCounter] = useState(value);
 
+    const isControlled = useRef(!!onChange)
+
     const handleIncreaseBy = (value: number) => {
+        if (isControlled.current) {
+            return onChange!({ count: value, product })
+        }
         const newValue = Math.max(counter + value, 0)
         setCounter(newValue)
         onChange?.({ product, count: newValue })
