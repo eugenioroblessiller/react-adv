f610e39304b20ac409c031da0840998ee3412d7d
eugenio <eugenio@baliotech.com>
2022-10-06 22:26:33 -0500

    first implementation for shopping cart

--- src/02-components-patterns/hooks/useProduct.ts
+++ src/02-components-patterns/hooks/useProduct.ts
@@ -11,12 +11,8 @@ export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => 
 
     const [counter, setCounter] = useState(value);
 
-    const isControlled = useRef(!!onChange)
 
     const handleIncreaseBy = (value: number) => {
-        if (isControlled.current) {
-            return onChange!({ count: value, product })
-        }
         const newValue = Math.max(counter + value, 0)
         setCounter(newValue)
         onChange?.({ product, count: newValue })
