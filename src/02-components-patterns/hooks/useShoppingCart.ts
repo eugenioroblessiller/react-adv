5a6c65a4e8af77a4467fe262a6eca0d78d209d41
eugenio <eugenio@baliotech.com>
2022-10-06 22:22:11 -0500

    new hook

---
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -0,0 +1,38 @@
+import { Product } from './../interfaces/interfaces';
+import React, { useState } from 'react'
+import { ProductInCart } from '../interfaces/interfaces';
+
+export const useShoppingCart = () => {
+ 
+    const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
+
+    const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
+        setShoppingCart(oldShoppingCart => {
+
+            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 }
+            if (Math.max(productInCart.count + count, 0) > 0) {
+                productInCart.count += count
+                return { ...oldShoppingCart, [product.id]: productInCart }
+            }
+
+            // Borrado de producto
+            const { [product.id]: toDelete, ...rest } = oldShoppingCart
+            return rest
+
+            // if (count === 0) {
+            //   const { [product.id]: toDelete, ...rest } = oldShoppingCart
+            //   return rest
+            // }
+            // return {
+            //   ...oldShoppingCart,
+            //   [product.id]: { ...product, count }
+            // }
+        })
+
+    };
+
+    return  {
+        shoppingCart,
+        onProductCountChange,
+    }
+}
